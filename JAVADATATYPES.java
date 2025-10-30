bef4d04e1dad233ba2bd05f2a415e313b48c505a
AMRENDRA RAJ <araj1@ce.iitr.ac.in>
2025-10-30 17:35:33 +0530

    Today's learning is Datatypes

---
+++ JAVADATATYPES.java
@@ -0,0 +1,29 @@
+//public class JAVADATATYPES {
+//    static void main(String args[]) {
+////        int a=1000000000;
+////        long b=a;
+////        long c=56781234567894567l;
+////        int d=(int) c;
+////        System.out.println(b);
+//
+////        float g= 222.98256784904f;
+////        double h=g;
+////        double k=24455.2345667898765432134567865432;
+////        float d=(float)k;
+////        System.out.println(d);
+////
+////        System.out.println(Float.MAX_VALUE);
+////        System.out.println(Double.MAX_VALUE);
+//
+////       for(int a=1; a<129; a++)
+////       {
+////           System.out.println((char) a );
+////       }
+//
+//
+//
+//
+//
+//
+//    }
+//}
