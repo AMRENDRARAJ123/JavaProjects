5298b2bc60c9b3977e38aa7f71e304588409a180
AMRENDRA RAJ <araj1@ce.iitr.ac.in>
2025-11-02 21:38:19 +0530

    Learned concatation

---
+++ Concatation.java
@@ -0,0 +1,13 @@
+package JavaProjects;
+
+public class Concatation {
+
+    static void main() {
+
+        int a = 1;
+        int b = 2;
+        String c = "sum";
+
+        System.out.println(c + " of" + " a+" + "b=" + (a + b));
+    }
+}
